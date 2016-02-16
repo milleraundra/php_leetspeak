579cd041a972f5f2df4c63b605ad6fa133c297d0
aundra miller and matt knutson <mknut001@gmail.com>
2016-02-15 16:31:27 -0800

    everything works

--- src/LeetspeakGenerator.php
+++ src/LeetspeakGenerator.php
@@ -2,15 +2,40 @@
 
     class LeetspeakGenerator
     {
-        function makeLeetspeak($input_phrase)
-        {
+        function makeLeetspeak($input_phrase) {
+            $input_array_of_words = explode(" ", $input_phrase);
+            $final_phrase = array();
 
+                for ($i=0; $i < count($input_array_of_words); $i++) {
+                    $word = $input_array_of_words[$i];
+                    $input_array_of_letters = str_split($input_array_of_words[$i]);
+                    $final_word = array();
+
+                    foreach($input_array_of_letters as $index => $letter) {
+                        if ($letter == "e") {
+                            array_push($final_word, "3");
+                        }
+
+                        elseif ($letter == "o"){
+                            array_push($final_word, "0");
+                        }
+
+                        elseif ($letter == "I"){
+                            array_push($final_word, "1");
+                        }
+
+                        elseif ($index != 0 && $letter == "s") {
+                            array_push($final_word, "z");
+                        }
+
+                        else {
+                            array_push($final_word, $letter);
+                        }
+                    }
+                    array_push($final_phrase, implode("", $final_word));
+            }
+            return implode(" ", $final_phrase);
         }
     }
 
  ?>
-
-
- <!-- // if (gettype($output_titlecased[0] == "integer")) {
- //     $output_titlecased[0] = ucfirst($output_titlecased[0]);
- // } -->
--- tests/LeetspeakGeneratorTest.php
+++ tests/LeetspeakGeneratorTest.php
@@ -1,18 +1,46 @@
 <?php
 
-    require_once "src/TitleCaseGenerator.php";
+    require_once "src/LeetspeakGenerator.php";
 
-    class TitleCaseGeneratorTest extends PHPUnit_Framework_TestCase
+    class LeetspeakGeneratorTest extends PHPUnit_Framework_TestCase
     {
-        function test_makeTitleCase_allLower()
+        function test_makeLeetspeak_replaceEwith3()
         {
-            $test_TitleCaseGenerator = new TitleCaseGenerator;
-            $input = "ALL UPPER";
+            $test_LeetspeakGenerator = new LeetspeakGenerator;
+            $input = "me";
 
-            $result = $test_TitleCaseGenerator->makeTitleCase($input);
+            $result = $test_LeetspeakGenerator->makeLeetspeak($input);
 
-            $this->assertEquals("All Upper", $result);
+            $this->assertEquals("m3", $result);
+        }
+        function test_makeLeetspeak_replaceIwith1()
+        {
+            $test_LeetspeakGenerator = new LeetspeakGenerator;
+            $input = "Italy";
+
+            $result = $test_LeetspeakGenerator->makeLeetspeak($input);
+
+            $this->assertEquals("1taly", $result);
         }
 
+        function test_makeLeetspeak_replaceSwithZ()
+        {
+            $test_LeetspeakGenerator = new LeetspeakGenerator;
+            $input = "spaceship";
+
+            $result = $test_LeetspeakGenerator->makeLeetspeak($input);
+
+            $this->assertEquals("spac3zhip", $result);
+        }
+
+        function test_makeLeetspeak_multipleWords()
+        {
+            $test_LeetspeakGenerator = new LeetspeakGenerator;
+            $input = "The Italian spaceship is green";
+
+            $result = $test_LeetspeakGenerator->makeLeetspeak($input);
+
+            $this->assertEquals("Th3 1talian spac3zhip iz gr33n", $result);
+        }
     }
 ?>
